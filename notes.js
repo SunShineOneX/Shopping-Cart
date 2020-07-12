4ab77c8e01bbda765d744beff0e87325106fbddd
SunShineOneX <SunShineOneX@gmail.com>
2020-07-12 19:35:59 -0400

    project upload

---
+++ notes.js
@@ -0,0 +1,32 @@
+/* Things we need:
+
+Items (2)
+Left side of the example
+========================
+Each one should have:
+    -the name of the item
+    -the price of the item
+
+
+Right side of the example
+==========================
+Things the cart is holding on to:
+    1. subtotal
+    2. delivery
+    3. estimated tax
+    4. order summary
+    5. total
+    6. items in cart
+
+    Values that change
+    =================
+
+    1. quantity
+    2. subtotal
+    3. estimated tax
+    4. total total
+
+    1. total
+    2. quantitiy of the items
+    3. subtotal
+    4. estimated tax: $1.05
---
+++ src/components/Shoppingcart.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+
+  class Shoppingcart extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            items: [
+                {
+                    //ITEM1
+                    quantity: 0,
+                    price: 5,
+                    name: "Cheese dogs"
+                },
+                {
+                    //ITEM2
+                    quantity: 0,
+                    price: 69.95,
+                    name: "Diablo 4"
+                }
+            ],
+
+            cartSubTotal: 0,
+            tax: 0.07,
+            total: 0,
+        }
+    
+    }
+    // Components
+    addQuantity = index => {
+        this.setState(
+            {
+        quantity: this.state.items[index].quantity ++
+        }
+    )
+}
+
+    subtractQuantity = index => {
+        this.setState(
+            {
+        quantity: this.state.items[index].quantity --
+        }
+    )
+}
+
+    render() {
+
+        //items
+        let cheeseDog = this.state.items[0];
+        let Diablo = this.state.items[1];
+        
+        //Calculations 
+        let itemTotalCount = cheeseDog.quantity + Diablo.quantity;
+        let cheeseDogCount = cheeseDog.quantity;
+        let DiabloCount = Diablo.quantity;
+        let cartSubTotal = Math.floor((cheeseDogCount * cheeseDog.price) + (DiabloCount * Diablo.price));
+        let tax = (cartSubTotal * this.state.tax).toFixed(2);
+
+        // Total
+        let total = Math.floor(tax + cartSubTotal);
+        
+        return (
+            <div className="wrapper">
+                <div className="items">
+                    <div className="firstItem">
+                        <h2>Item name:</h2>
+                        <p>{cheeseDog.name}</p>
+                        <h4>Quantity: {cheeseDog.quantity}</h4>
+                        <button onClick={() =>this.subtractQuantity(0)}>-</button> 
+                        <button onClick={() =>this.addQuantity(0)}>+</button>
+                        <h4>Price: ${cheeseDog.price}</h4>
+                        
+                    </div>
+                    <div className="secondItem">
+                        <h2>Item name:</h2>
+                        <p>{Diablo.name}</p>
+                        <h4>Quantity: {Diablo.quantity}</h4>
+                        <button onClick={() =>{this.subtractQuantity(1)}}>-</button>
+                        <button onClick={() =>{this.addQuantity(1)}}>+</button>
+                        <h4>Price: ${Diablo.price}</h4>
+                    </div>
+                </div>
+                    <div className="checkOut">
+                        <h2>{itemTotalCount}</h2>
+                        <h1>${cartSubTotal}</h1>
+                        <h2>Tax: {tax}</h2>
+                        <h1>Total: {total}</h1>
+                    </div>
+            </div>
+        )
+    }
+  }
+export default Shoppingcart;
+
